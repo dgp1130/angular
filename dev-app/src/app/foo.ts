2218718494d3cc00da9dc9af094ddd003075e47c
Doug Parker <dgp1130@users.noreply.github.com>
2025-11-06 14:37:11 -0800

    wip: `dev-app` test case

---
+++ dev-app/src/app/foo.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-foo',
+    template: `
+        <div>Hello from foo</div>
+    `,
+    styles: `
+        div {
+            color: red;
+        }
+    `,
+})
+export class Foo {}
--- dev-app/src/main.ts
+++ dev-app/src/main.ts
@@ -1,5 +1,10 @@
-import {bootstrapApplication} from '@angular/platform-browser';
+import {createApplication} from '@angular/platform-browser';
 import {App} from './app/app';
 import {appConfig} from './app/app.config';
 
-bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+(async () => {
+  const appRef = await createApplication(appConfig);
+  const container = document.getElementById('container')!;
+  const root = container.shadowRoot!.querySelector('app-root');
+  appRef.bootstrap(App, root);
+})();
