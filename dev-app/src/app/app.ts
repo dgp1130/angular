500e18d141b8807f6a501fca0bb1d045a6bb81a8
Doug Parker <dgp1130@users.noreply.github.com>
2025-11-06 14:37:11 -0800

    wip: `dev-app` test case

--- dev-app/src/app/app.ts
+++ dev-app/src/app/app.ts
@@ -1,12 +1,45 @@
-import {Component, signal} from '@angular/core';
+import {
+  ApplicationRef,
+  Component,
+  createComponent,
+  inject,
+  OnInit,
+  signal,
+  viewChild,
+  ViewContainerRef,
+} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {Foo} from './foo';
+import {NgComponentOutlet} from '@angular/common';
+import {Bar} from './bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Bar, Foo, RouterOutlet, NgComponentOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
+  private readonly appRef = inject(ApplicationRef);
+  private readonly container = viewChild.required('container', {
+    read: ViewContainerRef,
+  });
+
   protected readonly title = signal('dev-app');
+  protected readonly show = signal(false);
+  protected readonly foo = Foo;
+  protected readonly bars = signal([] as Array<{}>);
+
+  ngOnInit(): void {
+    const foo = createComponent(Foo, {environmentInjector: this.appRef.injector});
+    this.container().insert(foo.hostView);
+  }
+
+  protected addBar(): void {
+    this.bars.update((b) => [...b, {}]);
+  }
+
+  protected removeBar(): void {
+    this.bars.update((b) => b.slice(0, b.length - 1));
+  }
 }
